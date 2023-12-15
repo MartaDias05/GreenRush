37735082a4bcf3ff06e9359c7e19ecbc3a594faf
solomiiakoba <152209598+solomiiakoba@users.noreply.github.com>
2023-12-15 00:52:10 +0000

    + searchbar

--- GreenRush/script.js
+++ GreenRush/script.js
@@ -5,3 +5,18 @@ function goToSubenshi() {
 function goToMain() {
     location.replace('index.html')
 }
+function search() {
+    let filter = document.getElementById('find').value.toUpperCase();
+    let items = document.querySelectorAll('.listings-grid-element');
+
+    for (let i = 0; i < items.length; i++) {
+        let h5Element = items[i].querySelector('h5');
+        let value = h5Element.innerHTML || h5Element.innerText || h5Element.textContent;
+
+        if (value.toUpperCase().indexOf(filter) > -1) {
+            items[i].style.display = "";
+        } else {
+            items[i].style.display = "none";
+        }
+    }
+}
