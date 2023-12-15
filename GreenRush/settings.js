c8d3a16994f54a3c45c181cbd4b9a942b5e33bb2
LeTo <tomascostag@gmail.com>
2023-12-15 05:44:14 +0000

    comecei a implementar as definições em geral e de utilizador

---
+++ GreenRush/settings.js
@@ -0,0 +1,6 @@
+function hovering_effect(value){
+    document.getElementById(value).style.transform = 'scale(1.07)';
+}
+function reset_hovering_effect(value){
+    document.getElementById(value).style.transform = 'scale(1.0)';
+}
